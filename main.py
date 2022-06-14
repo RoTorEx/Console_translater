ceba99f93f7b372c395af54a52c5bb0dbd50f9bf
Aleksey Strelkov <aleks.strel.8v@gmail.com>
2022-06-14 13:25:48 +0300

    feat: add memory info
    Write RAM memory checker which show used memory by script's work.

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import os
+import psutil
+
 from base import WordsDictionary
 
 
+def stop():
+    process = psutil.Process(os.getpid())
+    memory = process.memory_info().rss
+    print(f"\nScript used \u001b[32m {memory//1024}\u001b[0m Kb of RAM.")
+
+    exit()
+
+
 def main():
+    try:
+        while True:
+            user_input = input("Enter num (Add, Random show or Guess words): ")
 
-    while True:
-        user_input = input("Enter num (Add, Random show or Guess words): ")
+            match user_input:
+                case "1":
+                    WordsDictionary.add()
 
-        match user_input:
-            case "1":
-                WordsDictionary.add()
-            case "2":
-                WordsDictionary.show()
-            case "3":
-                WordsDictionary.guess()
-            case "config":
-                WordsDictionary.config()
+                case "2":
+                    WordsDictionary.show()
 
-        print("Unsupported input, try again.")
+                case "3":
+                    WordsDictionary.guess()
+
+                case "config":
+                    WordsDictionary.config()
+
+            print("Unsupported input, try again.")
+
+    except KeyboardInterrupt:
+        stop()
 
 
 if __name__ == "__main__":
